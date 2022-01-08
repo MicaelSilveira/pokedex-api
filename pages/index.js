ec50f59cda16c763bf09b916841a481beb59a5e5
Micael <micaelsilveira31@gmail.com>
2022-01-07 21:16:23 -0300

    dropMenu

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Feed from "../src/Components/FeedPokemon/Feed";
 import styles from "../styles/Home.module.css";
 import React from "react";
+import Head from "next/head";
 export default function Home({ FeedList }) {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Pokedex</title>
+      </Head>
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Seja Bem vindo</h1>
       </div>
